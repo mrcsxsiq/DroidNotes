c4fcde82b4976f1195dde119808df104b8d1bd76
Marcos Paulo Farias <marcosfarias.dev@gmail.com>
2021-07-19 02:33:59 -0300

    fix project libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,27 +49,37 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
     }
+
 }
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:3.17.3"
+        artifact = if (osdetector.os == "osx") {
+            "com.google.protobuf:protoc:3.17.3:osx-x86_64"
+        } else {
+            "com.google.protobuf:protoc:3.17.3"
+        }
     }
 
     plugins {
+        val isMacM1 = if (osdetector.os == "osx") { ":osx-x86_64" } else { "" }
         id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.39.0"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.39.0$isMacM1"
         }
-        id("grpckt") {
-            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.1.0:jdk7@jar"
+        id("javalite") {
+            artifact = "com.google.protobuf:protoc-gen-javalite:3.0.0$isMacM1"
         }
     }
 
     generateProtoTasks {
         all().forEach {
             it.plugins {
-                id("grpc")
-                id("grpckt")
+                id("java") {
+                    option("lite")
+                }
+                id("grpc") {
+                    option("lite")
+                }
             }
         }
     }
@@ -77,26 +87,24 @@ protobuf {
 
 dependencies {
 
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material-icons-extended:${rootProject.extra["compose_version"]}")
-
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
-    implementation("com.google.protobuf:protobuf-lite:3.0.1")
-    implementation("io.grpc:grpc-okhttp:1.21.1")
-    implementation("io.grpc:grpc-protobuf-lite:1.21.1")
-    implementation("io.grpc:grpc-stub:1.21.1")
-    implementation("io.grpc:grpc-netty-shade:1.21.1")
-
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.0-rc02")
 
-    testImplementation("junit:junit:4.13.2")
+    // gRPC
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+    implementation("io.grpc:grpc-okhttp:1.39.0")
+    implementation("io.grpc:grpc-protobuf-lite:1.39.0")
+    implementation("io.grpc:grpc-stub:1.39.0")
 
+    // Tests
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
