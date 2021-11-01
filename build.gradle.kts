e0aabb2fb6d361c1e1d4e0cb9c28b15a8f4f8dd6
Marcos Paulo Farias <marcosfarias.dev@gmail.com>
2021-11-01 01:53:06 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,25 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
+buildscript.  {
     val compose_version by extra("1.0.0-rc02")
-    var room_version by extra("2.4.0-alpha03")
+    var room_version by  extra("2.4.0-alpha03")
 
     repositories {
         google()
-        mavenCentral()
+         mavenCentral()
     }
+    
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.17")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.17" )
+        classpath( "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     }
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+
+
+
